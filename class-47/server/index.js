6c719a68327228b3417f6583374918f234791d02
Shashwat Bagaria <shashwawtforwork@gmail.com>
2025-03-30 08:10:04 +0530

    Deploy 1

--- class-47/server/index.js
+++ class-47/server/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 const app = express();
 
 require("dotenv").config();
@@ -11,8 +14,40 @@ const theatreRoutes = require("./routes/theatreRoute");
 const showRoutes = require("./routes/showRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 
+// Use helmet for setting various HTTP headers for security
+app.use(helmet());
+
+// Custom Content Security Policy (CSP) configuration
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "example.com", "scaler.com"], // Allow scripts from 'self' and example.com
+      styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles (unsafe)
+      imgSrc: ["'self'", "data:", "example.com"], // Allow images from 'self', data URLs, and example.com
+      connectSrc: ["'self'", "api.example.com"], // Allow connections to 'self' and api.example.com
+      fontSrc: ["'self'", "fonts.gstatic.com"], // Allow fonts from 'self' and fonts.gstatic.com
+      objectSrc: ["'none'"], // Disallow object, embed, and applet elements
+      upgradeInsecureRequests: [], // Upgrade insecure requests to HTTPS
+    },
+  })
+);
+
+// Rate Limiter middleware
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per WindowMs
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+// Sanitize user input to prevent MongoDB Operator Injection
+app.use(mongoSanitize());
+
 app.use(cors());
 app.use(express.json());
+
+// Apply rate limiter to all API routes
+app.use("/api/", apiLimiter);
 
 app.use("/api/users", userRoutes);
 app.use("/api/movies", movieRoutes);
