e76918c03ad35894e9ec5355b8df75f728574240
Shashwat Bagaria <shashwawtforwork@gmail.com>
2025-03-30 08:27:09 +0530

    Deploy 3

--- class-47/client/src/apicalls/index.js
+++ class-47/client/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://scaler-shows.onrender.com/",
+  baseURL: "https://scaler-shows.onrender.com",
   headers: {
     withCredentials: true,
     "Content-Type": "application/json",
